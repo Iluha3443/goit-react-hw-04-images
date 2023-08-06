8b37d0effbd4069a5e119ef0d33dcfb8e3a48421
Iluha3443 <tkacenkoila01@gmail.com>
2023-08-06 20:40:08 +0300

    task

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { Component,useState,useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Loader from '../Loader/Loader';
@@ -6,43 +6,12 @@ import Button from '../Button/Button';
 import { nanoid } from 'nanoid';
 import PropTypes  from 'prop-types';
 
-const ImageGallery = ({query,page,perPage,onLoadMore,onOpenModal}) => {
-
+const ImageGallery = ({ query, page, perPage, onLoadMore, onOpenModal }) => {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [totalImages, setTotalImages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  // const [perPage, setPerPage] = useState(12);
-
-  useEffect(() => {
-    fetchImages();
-    addScrollListener();
-  }, []);
-  
-
-  useEffect(prevProps => {
-  if (prevProps.query !== query) {
-    setImages([]);
-    setIsLoading(false);
-    setCurrentPage(1);
-    fetchImages();
-  }
-}, [query]);
-  
-    useEffect(() => {
-      return () => {
-      removeScrollListener()
-    }
-    }, []);
-  
-
- const addScrollListener = () => {
-    window.addEventListener('load', handleScroll);
-  };
-
- const removeScrollListener = () => {
-    window.removeEventListener('load',handleScroll);
-  };
+  const [perPages, setPerPages] = useState(12);
 
   const handleScroll = () => {
     if (!isLoading && images.length < totalImages) {
@@ -56,10 +25,35 @@ const ImageGallery = ({query,page,perPage,onLoadMore,onOpenModal}) => {
     }
   };
 
- const fetchImages = () => {
+  useEffect(() => {
+    setImages([])
+    setIsLoading(false)
+    setCurrentPage(1)
+    fetchImages();
+  }, [query]);
+
+  useEffect(() => {
+    fetchImages();
+    addScrollListener();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      removeScrollListener()
+    }
+  }, []);
+  
+  const addScrollListener = () => {
+    window.addEventListener('load', handleScroll);
+  };
+
+  const removeScrollListener = () => {
+    window.removeEventListener('load', handleScroll);
+  };
+    
+  const fetchImages = () => {
     const apiKey = '37446225-ced4f53dd81a7d760f8a029fd';
     const url = `https://pixabay.com/api/?q=${query}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
-
     setIsLoading(true);
 
     axios
@@ -69,33 +63,33 @@ const ImageGallery = ({query,page,perPage,onLoadMore,onOpenModal}) => {
           ...image,
           id: nanoid(),
         }));
-
-        setImages(prevState => [...prevState.images, ...newImages]);
-        setIsLoading(false);
-        setTotalImages(response.data.total);
-        setCurrentPage(prevState => (prevState.currentPage + 1))
+        setImages(prevState => [...prevState, ...newImages])
+        setIsLoading(false)
+        setTotalImages(response.data.total)
+        setCurrentPage(1)
+        setCurrentPage(prevState => prevState + 1)
       })
       .catch(error => {
         console.error('Error fetching images:', error);
-        setIsLoading(false);
+        setIsLoading(false)
       });
   };
 
-    return (
-      <div>
-        <ul className="ImageGallery">
-          {images.map(image => (
-            <ImageGalleryItem key={image.id} image={image} onOpenModal={onOpenModal} />
-          ))}
-        </ul>
-        {isLoading && <Loader />}
-        {images.length > 0 && images.length < totalImages && (
-          <Button onClick={fetchImages} hasMore={!isLoading} />
-        )}
-      </div>
-    );
-  }
 
+  return (
+    <div>
+      <ul className="ImageGallery">
+        {images.map(image => (
+          <ImageGalleryItem key={image.id} image={image} onOpenModal={onOpenModal} />
+        ))}
+      </ul>
+      {isLoading && <Loader />}
+      {images.length > 0 && images.length < totalImages && (
+        <Button onClick={fetchImages} hasMore={!isLoading} />
+      )}
+    </div>
+  );
+};
 
 ImageGallery.propTypes = {
   query: PropTypes.string.isRequired,
